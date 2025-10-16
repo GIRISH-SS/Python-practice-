a48dadfa5ba349ac154b20fbf917b1fe77873bbd
GIRISH-SS <girisha.s123s@gmail.com>
2025-10-16 17:45:53 +0530

    mutable/immutable

---
+++ 4_mutable_&_immutable(data_types)/mutable/mutable.py
@@ -0,0 +1,10 @@
+lst = [10, 20, "apple", False, 3+5j]
+print(lst)
+print(type(lst))
+print(id(lst))
+lst.append(30)
+
+lst1 = [10, 20, "apple", False, 3+5j, 30]
+print(lst1)
+print(type(lst1))
+print(id(lst1))
